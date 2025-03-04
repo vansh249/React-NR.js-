01a3a9858b94749839aa6db9f12ca70ffa694f33
Vansh Verma <vanshverma@Vanshs-MacBook-Air.local>
2025-03-04 07:35:47 +0530

    ep02

---
+++ React/Chapter 02/app.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello world from REACT!!"
+);
+console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I'm a h1"),
+      React.createElement("h2", {}, "I'm a h2"),
+    ]),
+  ],
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I'm a h1"),
+      React.createElement("h2", {}, "I'm a h2"),
+    ]),
+  ],
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
