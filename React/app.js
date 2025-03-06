27510444b4f609ae1a8396aa935923beeeebbd2b
Vansh Verma <vanshverma@Vanshs-MacBook-Air.local>
2025-03-06 07:08:40 +0530

    ch03

--- React/app.js
+++ React/app.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = <h1 className="heading">Hello from JSX</h1>;
+const Title = () => <h1>Hello from React and JSX</h1>;
+
+const Heading = () => (
+  <div id="container">
+    <Title />
+    <h1>This is a Heading</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(heading);
+
+root.render(<Heading />);
